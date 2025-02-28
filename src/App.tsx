99eb22ebca540cb43a1ef272c67a8a91205aaaa9
Alacoque <kawaalacoque@gmail.com>
2025-02-28 12:50:29 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,21 @@ import { About } from './components/About';
 import { Projects } from './components/Projects';
 import { Contact } from './components/Contact';
 import { Footer } from './components/Footer';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD6vEogyX9lya0IO0RneduaKTYlIUzFjg4",
+  authDomain: "projeto-teste-b7d98.firebaseapp.com",
+  projectId: "projeto-teste-b7d98",
+  storageBucket: "projeto-teste-b7d98.firebasestorage.app",
+  messagingSenderId: "795464276336",
+  appId: "1:795464276336:web:a2d1e1f5db9a23cfda97b5",
+  measurementId: "G-FXJYYHS9K6"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 export function App() {
   return (
